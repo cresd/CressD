929bee99627b89017762cd3a78552406bc63e415
cresd <creesd.bayati@gmail.com>
2016-05-08 02:53:02 -0800

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,14 +245,14 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {90516804},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 https://github.com/BH-YAGHI/NOD32-BOT.git
 
-channel : @Nod32team
-sodu : @behrooZyaghi
+
+sodu : @CreesD
 ]],
     help_text_realm = [[
 Realm Commands:
